68eed50c98eaec888b14d81ac30d5d2ae2e8f175
luiz eduardo da cruz cruz <luizeduardodacruzcruzluiz@hotmail.com>
2018-09-07 22:36:49 -0300

    khkhk

---
+++ Frei.Marcos.Sigma/Frei.Marcos.Sigma/DB/Veiculo/VeiculoBusiness.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+class VeiculoBusiness
+{
+    public int CadastrarVeiculo(VeiculoDTO dto)
+    {
+        VeiculoDatabase db = new VeiculoDatabase();
+        return db.CadastrarFuncionario(dto);
+    }
+
+    public List<VeiculoDTO> ConsultarVeiculo(string Modelo)
+    {
+        VeiculoDatabase db = new VeiculoDatabase();
+        if (Modelo != string.Empty)
+        {
+            return db.ConsultarVeiculoFiltro(Modelo);
+        }
+        else
+            return db.ConsultaVeiculo();
+    }
+    public int RemoverVeiculo(string id)
+    {
+        VeiculoDatabase db = new VeiculoDatabase();
+        return db.RemoverFuncionario(id);
+    }
+    public int AlterarVeiculo(VeiculoDTO dto)
+    {
+        VeiculoDatabase db = new VeiculoDatabase();
+        return db.AlterarVeiculo(dto);
+    }
+}
