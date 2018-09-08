31f31579e7c908d8f9c5ad0256f854bead0f0d8b
luiz eduardo da cruz cruz <luizeduardodacruzcruzluiz@hotmail.com>
2018-09-08 12:26:50 -0300

    afas

---
+++ Frei.Marcos.Sigma/Frei.Marcos.Sigma/Modulos/ControleDeCliente/frmClienteAlterar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frei.Marcos.Sigma.Modulos.ControleDeCliente
+{
+    public partial class frmClienteAlterar : Form
+    {
+        public frmClienteAlterar()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Frei.Marcos.Sigma/Frei.Marcos.Sigma/Modulos/ControleDeCliente/frmClienteConsultar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frei.Marcos.Sigma.Modulos.ControleDeCliente
+{
+    public partial class frmClienteConsultar : UserControl
+    {
+        public frmClienteConsultar()
+        {
+            InitializeComponent();
+        }
+    }
+}
