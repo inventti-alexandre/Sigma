df1eb5e9a083533e63152eceeac0b4bc2aaf3e05
luiz eduardo da cruz cruz <luizeduardodacruzcruzluiz@hotmail.com>
2018-09-07 22:02:44 -0300

    das

---
+++ Frei.Marcos.Sigma/Frei.Marcos.Sigma/Modulos/ControleDeCliente/frmCadastrar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+	public partial class frmCadastrar : FROM
+    {
+        public frmCadastrar()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClienteBusiness dto = new ClienteBusiness();
+                dto.Nome = txtName.Text;
+                dto.Usuario = txtUsuario.Text;
+                dto.Cargo = txtCargo.Text;
+                dto.Complemento = txtComplemento.Text;
+                dto.CPF = txtCPF.Text;
+                dto.Data_nascimento = dtpNasc.Value;
+                dto.Endereco = txtEndereco.Text;
+                dto.observacao = txtObs.Text;
+                dto.RG = txtRG.Text;
+
+                FuncionarioBusiness business = new FuncionarioBusiness();
+                business.CadastrarFuncionario(dto);
+
+                MessageBox.Show("Funcionario Cadastrado", "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message, "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string CEP = txtCEP.Text;
+
+                CorreioBusiness business = new CorreioBusiness();
+                CorreioDTO dto = business.Endereco(CEP);
+
+                if (dto.CEP != string.Empty)
+                {
+                    txtComplemento.Text = dto.Complemento;
+                    txtEndereco.Text = $"Logradouro: {dto.Logradouro}, Cidade: {dto.Localidade}, Bairro: {dto.Bairro}, UF: {dto.UF}";
+                }
+                else
+                    throw new ArgumentException("CEP Inválido");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
---
+++ Frei.Marcos.Sigma/Frei.Marcos.Sigma/Modulos/ControleDeCliente/frmalterar.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public partial class frmAlterar : FROM
+{
+    public frmAlterar()
+    {
+        InitializeComponent();
+
+    }
+     public void CarregarCampos(int id, string CPFCNPJ,string nome, string end, string bairro, DateTime dt_nasc, string rg,string celular,string telefone,string estado,string cidade,string cep)
+    {
+        lblId.Text = id.ToString();
+        txtCargo.Text = usuario;
+        txtComplemento.Text =celular ;
+        txtCPF.Text = CPFCNPJ;
+        txtEndereco.Text = end;
+        txtName.Text = nome;
+        txttelefone.Text = telefone;
+        txtUsuario.Text = usuario;
+        txtRG.Text = rg;
+        txtbairro = bairro;
+        DateTime.text = dt_nasc;
+        txtestado = estado;
+        txtcidade = cidade;
+        txtcep = cep;
+    }
+
+    private void btnAlterar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            lblId.Text = id.ToString();
+            txtCargo.Text = usuario;
+            txtComplemento.Text = celular;
+            txtCPF.Text = CPFCNPJ;
+            txtEndereco.Text = end;
+            txtName.Text = nome;
+            txttelefone.Text = telefone;
+            txtUsuario.Text = usuario;
+            txtRG.Text = rg;
+            txtbairro = bairro;
+            DateTime.text = dt_nasc;
+            txtestado = estado;
+            txtcidade = cidade;
+            txtcep = cep;
+
+            ClienteBusiness business = new ClinteBusiness();
+            business.AlterarCliente(dto);
+
+            MessageBox.Show("Cliente alterado", "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (ArgumentException ex)
+        {
+            MessageBox.Show(ex.Message, "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Ocorreu um erro: " + ex.Message, "SIGMA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private void frmAlterar_Load(object sender, EventArgs e)
+    {
+
+    }
+}
+   
+         
+    
+
+
+
